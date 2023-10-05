04bd5050762b9e55978b4479f29a98be0d6ef795
jo <yuyule20@naver.com>
2023-10-05 12:47:17 +0900

    2023 - 10 - 05

--- src/main/java/day6/ch4/While.java
+++ src/main/java/day6/ch4/While.java
@@ -3,5 +3,19 @@ package day6.ch4;
 public class While {
     public static void main(String[] args) {
         // while 반복문
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println("for - " + i);
+        }
+
+        System.out.println("-----------------------------");
+        // 동일
+
+        int i = 0;
+        while (i < 10) {
+            System.out.println("while - " + i++);
+
+        }
+
     }
 }
